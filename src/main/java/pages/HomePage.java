fa4ce6be53a9eafa6475c0e06d4a2252714facd4
TofaelKabir <tofaelkabir@gmail.com>
2023-11-07 22:04:24 -0500

    CMS Portal Framework Day01

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,70 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+	WebDriver driver;
+
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		// PageFactory help to instantiate WebElements
+		PageFactory.initElements(driver, this);
+	}
+	
+	// 1st way: Most common way to write the WebElement (@FindBy), you must know how to do that
+	// Use of unique 'Id' attribute as locator
+	@FindBy (id = "cms-login-submit")
+	WebElement loginButton;
+	
+	@FindBy (name = "user-d")
+	WebElement userId;
+	
+	// 2nd way to write the WebElement: not common, here I used for the 'password'
+	// Just to make you guys familiar, if you see in your job
+	// Use of unique 'name' attribute as locator
+	@FindBy (how = How.NAME, using = "pass-d")
+	WebElement password;
+	
+	// Use of unique  'class' attribute as locator
+	// FYI: Never take a class value as unique if they have white space between words. 
+	// Example: class value of user id, this is unique, but a compound class --> "cms-login-field ng-dirty ng-touched ng-invalid"
+	@FindBy (className = "cms-newuser-reg")
+	WebElement newUserRegistration;
+	
+	@FindBy (xpath = "//em[@id='cms-homepage-header-logo-unauth' and @class='cms-icon cms-sprite-loggedout ms-3']")
+	WebElement logo;
+	
+	public void clickLoginButton() throws InterruptedException {
+		loginButton.click();
+		Thread.sleep(5000);
+	}
+	
+	public void clickUserId () {
+		userId.click();
+	}
+	
+	public void clickPassword () {
+		password.click();
+	}
+	
+	public void clickNewUserRegistration() {
+		newUserRegistration.click();
+		// We used try catch block to throw exception
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void clickLogo() {
+		logo.click();
+	}
+	
+	
+
+}
