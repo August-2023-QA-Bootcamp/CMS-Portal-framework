95f9fb03acbea62d5c8bb2a9a3c620ed64f9cccf
TofaelKabir <tofaelkabir@gmail.com>
2023-11-14 21:58:19 -0500

    CMS Portal Framework Day03

--- src/test/java/pagesTest/HomePageTest.java
+++ src/test/java/pagesTest/HomePageTest.java
@@ -1,5 +1,6 @@
 package pagesTest;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import baseUtil.BaseClass;
@@ -31,14 +32,228 @@ public class HomePageTest extends BaseClass {
 		homePage.clickNewUserRegistration();
 	}
 	
-	@Test (enabled = true)
-	public void clickLogoTest() {
+	@Test (enabled = false)
+	public void clickLogoTest() throws InterruptedException {
 		homePage.clickLogo();
+	}
+	
+	@Test(enabled = false)
+	public void inputTextInUserIdFieldTest() {
+		homePage.inputTextInUserIdField();
+	}
+	
+	@Test(enabled = false)
+	public void clickForgotUserIdTest() {
+		homePage.clickForgotUserId();
+	}
+	
+	// Test for implicit wait and it shows No such element exception
+	@Test(enabled = false)
+	public void clickWrongNewUserIdTest() {
+		homePage.clickWrongNewUserRegistration();
+	}
+	
+	// This test case will fail, when you remove Pagefactory class from code form HomePage to test this
+	// When PageFactory class is absent, you will find NullPointerException
+	@Test (enabled = false)
+	public void nullPointerExceptionTest() throws InterruptedException {
+		homePage.clickNewUserRegistration();
+	}
+	
+	@Test(enabled = false)
+	public void inputTextInUserIdAndPasswordFieldThenClickLoginButtonTest() throws InterruptedException {
+		homePage.inputTextInUserIdAndPasswordFieldThenClickLoginButton();
+	}
+	
+	@Test(enabled = false)
+	public void clickUnlockTest() throws InterruptedException {
+		homePage.clickUnlock();
 	}
 	
 	
 	
 	
+	
+	// From here, we are not creating method in HomePage Class to test
+	
+	// Important: Use of xpath as locator
+	// Web Element: New User Registration Button
+	// We can use a web element directly in the test class, that is also common, but different people like different way
+	@Test (enabled = false)
+	public void use_of_xpath_as_locator_in_new_user_registration_button() throws InterruptedException {
+		driver.findElement(By.xpath("//a[contains(text(), 'New')]")).click();
+		Thread.sleep(4000);
+	}
+	
+
+	// This is best scripted in ebay (I will show it later)
+	// Important: Use of linkText as locator
+	// Web Element: forgot User Id
+	@Test(enabled = false)
+	public void use_of_linkText_as_locator_in_forgot_user_id_hyperlink() throws InterruptedException {
+		Thread.sleep(4000);
+		driver.findElement(By.linkText("User ID")).click();
+		Thread.sleep(4000);
+	}
+	
+	// Important: Use of partialLinkText as locator
+	// Web Element: forgot Password
+	@Test(enabled = false)
+	public void use_of_partialLinkText_as_locator_in_forgot_password_hyperlink() throws InterruptedException {
+		Thread.sleep(5000);
+		driver.findElement(By.partialLinkText("Passwor")).click();
+		Thread.sleep(4000);
+	}
+	
+	// logo: class attribute value:  cms-icon cms-sprite-loggedout ms-3
+	// Exception 'InvalidSelectorException': 'Compound class names not permitted',
+	// so even if you see class value is unique but separated by space, avoid this as class name locator
+	// This scenario will show error and will failed everywhere
+
+	@Test(enabled = false)
+	public void why_we_use_cssSelector_as_locator_in_logo () {
+		driver.findElement(By.className("cms-icon cms-sprite-loggedout ms-3")).click();
+	}
+
+	// Web Element: New User Registration Button
+	// Important: Use of cssSelector as locator
+	// cssSelector is very important for interview
+	// cssSelector by class --> inside the string, first put html tag, then dot, then write value of the class attribute 
+	@Test(enabled = false)
+	public void role1_use_of_cssSelector_as_locator_in_new_user_registration_button () {
+		driver.findElement(By.cssSelector("a.cms-newuser-reg")).click();
+	}
+	
+	// Important: Use of cssSelector as locator
+	// Web Element: login Button
+	// if you wanna create cssSelector by id 
+	// first html tag, then # (hash), then write the value of id attribute inside the string
+	@Test(enabled = false)
+	public void role2_use_of_cssSelector_as_locator_in_login_button() {
+		driver.findElement(By.cssSelector("button#cms-login-submit")).click();
+	}
+	
+	// Web Element: logo
+	// Important: Use of cssSelector as locator
+	// if the class (compound class) contain separate words [cms-icon cms-sprite-loggedout ms-3], 
+	// they are actually different class, then
+	// we have to close the gap between classes by putting dot/period between them
+	// cssSelector by class --> htmltag.class name value 
+	// remove the space between words in class value, and replace with dot/period
+	@Test(enabled = false)
+	public void role3_use_of_cssSelector_as_locator_in_logo () {
+		driver.findElement(By.cssSelector("em.cms-icon.cms-sprite-loggedout.ms-3")).click();
+	}
+	
+	// Above 3 is mostly used, 90% cases of css selector, also very common interview question
+	// below 3 high level, no need to see if you feel they are tough
+	
+	// Use of cssSelector as locator
+	// Web Element: login Button
+	// high level, you can ignore
+	// cssSelector by another attribute
+	// --> htmltag[id/class/name attribute = 'value of the attribute' ] , in xpath - you use // and @, which is absent here 
+	// and you can use any other attribute name except id and class
+	@Test(enabled = false)
+	public void role4_use_of_cssSelector_as_locator_in_login_button () {
+		driver.findElement(By.cssSelector("button[name='Submit Login']")).click();
+	}
+	
+	// Web Element: NUR Button
+	// Not important, can ignore
+	// cssSelector by another attribute
+	// --> htmltag.value of class[id/class/name attribute = 'value of the attribute'] , in xpath you use // and @, which is absent here and you can use attribute except id and class
+	// That's why we use title
+	@Test(enabled = false)
+	public void role5_use_of_cssSelector_as_locator_in_login_button() throws InterruptedException {
+		driver.findElement(By.cssSelector("a.cms-newuser-reg[title='New User Registration']")).click();
+		Thread.sleep(5000);
+	}
+	
+	// Important: Use of cssSelector as locator
+	// Web Element: login Button
+	// Not important, can ignore
+	// cssSelector by another attribute
+	// --> htmltag#value of ID[attribute name='value'] , in xpath you use // and @, which is absent here and you can use attribute except id and class
+	@Test(enabled = false)
+	public void role6_use_of_cssSelector_as_locator_in_login_button () {
+		driver.findElement(By.cssSelector("button#cms-login-submit[name='Submit Login']")).click();
+	}
+	
+	// Important: Use of tagName as locator
+	// tag name: we have to go to Amazon and look for table
+	
+	@Test (enabled = false)
+	public void use_of_tagName_as_locator() {
+		driver.findElement(By.tagName("header")).click(); // not a clickable element, just showed you
+	}
+	
+	
+	// Web Element: logo
+	// isDisplayed() is an alternate of click() method which is boolean type
+	// isDisplayed() is the method used to verify the presence of a web element within the web page.
+	// Use of isDisplayed() available- image, link, button, text field, check box etc.
+	@Test(enabled = false)
+	public void use_of_isDisplayed_in_logo_image() {
+		boolean elementDisplayed = driver.findElement(By.cssSelector("em.cms-icon.cms-sprite-loggedout.ms-3")).isDisplayed();
+		System.out.println("Is the Logo displayed? Ans: " + elementDisplayed);
+	}
+	
+	// Web Element: login Button
+	// isEnabled() is the method used to verify if the web element is enabled or
+	// disabled within the web page. isEnabled() is primarily used with buttons.
+	// Use of isEnabled(), a boolean type method
+	@Test(enabled = false)
+	public void use_of_isEnabled_in_login_button() {
+		boolean loginButtonEnabled = driver.findElement(By.name("Submit Login")).isEnabled();
+		System.out.println("Is the Login Button Enabled? Ans: " + loginButtonEnabled);
+	}
+	
+	// Web Element: check box in login page
+	@Test (enabled = false)
+	public void use_of_isSelected_in_check_box () throws InterruptedException {
+		boolean checkBoxSelected = driver.findElement(By.xpath("//label[@id='cms-label-tc']")).isSelected();
+		System.out.println("Is the CheckBox selected? Ans: " + checkBoxSelected);
+		driver.findElement(By.xpath("//label[@id='cms-label-tc']")).click();
+		Thread.sleep(5000);
+	}
+	
+	//TODO  Nasir: select No, evaluating No, select Yes, evaluating Yes, why Still False?
+	// Web Element: Radio button in Forgot User ID 
+	@Test (enabled = false)
+	public void use_of_isSelected_in_radio_button () throws InterruptedException {
+		// first click on Forgot User ID
+		homePage.clickForgotUserId();
+		Thread.sleep(4000);
+		// another role for creating xpath below
+		driver.findElement(By.xpath("(//label[@class='check-radio'])[2]")).click();
+		Thread.sleep(4000);
+		boolean radioButtonYesSelected = driver.findElement(By.xpath("(//label[@class='check-radio'])[2]")).isSelected();
+		System.out.println("Is the Radio button selected? Ans: " + radioButtonYesSelected);
+		driver.findElement(By.xpath("(//label[@class='check-radio'])[1]")).click();
+		Thread.sleep(4000);
+		boolean radioButtonNoSelected = driver.findElement(By.xpath("(//label[@class='check-radio'])[1]")).isSelected();
+		System.out.println("Is the Radio button selected? Ans: " + radioButtonNoSelected);
+	}
+	
+	@Test(enabled = true)
+	public void use_of_isEnabled_in_login_button02() throws InterruptedException {
+		driver.findElement(By.name("user-d")).isDisplayed();
+		driver.findElement(By.name("user-d")).sendKeys("August 2023 QA Bootcamp");
+		Thread.sleep(4000);
+		driver.findElement(By.name("pass-d")).isDisplayed();
+		driver.findElement(By.name("pass-d")).sendKeys("ABC@123");
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//label[@id='cms-label-tc']")).isSelected();
+		driver.findElement(By.xpath("//label[@id='cms-label-tc']")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.name("Submit Login")).isEnabled();
+		driver.findElement(By.name("Submit Login")).click();
+		Thread.sleep(4000);
+	}
+	
+
+
 	
 	
 	
